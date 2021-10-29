a84110ce725467fed6a34bfb8935e934f5e56656
Sigvard Johansen Seljelv <48127711+SigSel@users.noreply.github.com>
2021-10-29 20:45:32 +0200

    Scripts for automatic fetching and transformation of whiskeys.

---
+++ scripts/python_jsonfetch.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+url = "https://www.vinmonopolet.no/api/search?q=:relevance:visibleInSearch:true:mainCategory:brennevin:mainSubCategory:brennevin_whisky:mainCountry:japan&searchType=product&currentPage=0&fields=FULL&pageSize=100"
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'}
+
+resp = requests.get(url=url, headers=headers)
+print(resp.status_code)
+data = resp.json()
+
+with open('search.json', 'w') as f:
+    json.dump(data, f)
---
+++ scripts/transform_data.R
@@ -0,0 +1,38 @@
+library(jsonlite)
+library(tidyverse)
+library(writexl)
+
+ads <- map(paste0("search.json"), fromJSON)
+data_raw <- pluck(ads,1, "productSearchResult")
+data <- pluck(data_raw, "products")
+
+#Extract variables from the data
+data_f <- data %>% 
+  select(code, name, url, product_selection)#, status, buyable, inStockSupplier)
+
+#Add price to the data, price is element two in the price list
+numob <- nrow(data_f)
+price <- data %>% 
+  select(price)
+price <- unlist(price)
+price <- price[(nrow(data_f)+1):(nrow(data_f)*2)] 
+data_f <- cbind(data_f, price)
+
+#Add volume to the data, volume is the first element in volume list
+volume <- data %>% 
+  select(volume)
+volume <- unlist(volume)
+volume <- volume[1:numob] 
+data_f <- cbind(data_f, volume)
+
+im <- data %>% 
+  select(images)
+im <- unlist(im)
+images <- array()
+for (i in 1:nrow(data_f)) {
+  images[i] <- im[i*6]
+}
+data_f <- cbind(data_f, images)
+
+write.csv(data_f, file='JW_vinmon.csv')
+
