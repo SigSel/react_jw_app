226fd9bab0327e1179204123bef0098ff238e798
Sigvard Johansen Seljelv <48127711+SigSel@users.noreply.github.com>
2022-06-07 17:45:18 +0200

    Updated headers for python request

--- scripts/python_jsonfetch.py
+++ scripts/python_jsonfetch.py
@@ -2,7 +2,12 @@ import requests
 import json
 
 url = "https://www.vinmonopolet.no/api/search?q=:relevance:visibleInSearch:true:mainCategory:brennevin:mainSubCategory:brennevin_whisky:mainCountry:japan&searchType=product&currentPage=0&fields=FULL&pageSize=100"
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'}
+headers = {
+    'User-Agent':
+        'python_requests',
+    'Content-Type':
+        'application/json; charset=utf-8'
+}
 
 resp = requests.get(url=url, headers=headers)
 print(resp.status_code)
