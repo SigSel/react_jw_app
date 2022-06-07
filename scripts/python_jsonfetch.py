eb19c68e00742f46a94a8d611ce8bc9fae0562fb
Sigvard Johansen Seljelv <48127711+SigSel@users.noreply.github.com>
2022-06-07 18:53:54 +0200

    cloudscraper did not work, trying playwright

--- scripts/python_jsonfetch.py
+++ scripts/python_jsonfetch.py
@@ -1,12 +1,22 @@
-import cloudscraper
 import json
+from playwright.sync_api import sync_playwright
+
 
 url = "https://www.vinmonopolet.no/api/search?q=:relevance:visibleInSearch:true:mainCategory:brennevin:" \
       "mainSubCategory:brennevin_whisky:mainCountry:japan&searchType=product&currentPage=0&fields=FULL&pageSize=100"
 
-scraper = cloudscraper.create_scraper()
-data = scraper.get(url).text
-data = json.loads(data)
+with sync_playwright() as p:
+    # Webkit is fastest to start and hardest to detect
+    browser = p.webkit.launch(headless=True)
+
+    page = browser.new_page()
+    page.goto(url)
+
+    # Use evaluate instead of `content` not to import bs4 or lxml
+    html = page.evaluate('document.querySelector("pre").innerText')
+
+
+data = json.loads(html)
 
 with open('search.json', 'w') as f:
     json.dump(data, f)
