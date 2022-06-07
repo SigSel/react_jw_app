5d025c2974d1e078b314dc1e528dae64b4dfe3cd
Sigvard Johansen Seljelv <48127711+SigSel@users.noreply.github.com>
2022-06-07 18:37:58 +0200

    Changed from request lib to cloudscraper
     Looks like Cloudflare's anti-bot page (or IUAM) has been implemented by Vinmonopolet.

--- scripts/python_jsonfetch.py
+++ scripts/python_jsonfetch.py
@@ -1,17 +1,12 @@
-import requests
+import cloudscraper
 import json
 
-url = "https://www.vinmonopolet.no/api/search?q=:relevance:visibleInSearch:true:mainCategory:brennevin:mainSubCategory:brennevin_whisky:mainCountry:japan&searchType=product&currentPage=0&fields=FULL&pageSize=100"
-headers = {
-    'User-Agent':
-        'gh-actions',
-    'Content-Type':
-        'application/json; charset=utf-8'
-}
+url = "https://www.vinmonopolet.no/api/search?q=:relevance:visibleInSearch:true:mainCategory:brennevin:" \
+      "mainSubCategory:brennevin_whisky:mainCountry:japan&searchType=product&currentPage=0&fields=FULL&pageSize=100"
 
-resp = requests.get(url=url, headers=headers)
-print(resp.status_code)
-data = json.loads(resp.text)
+scraper = cloudscraper.create_scraper()
+data = scraper.get(url).text
+data = json.loads(data)
 
 with open('search.json', 'w') as f:
     json.dump(data, f)
