5f95822f7868a24b580d341673b6c6359a4f62a2
Sigvard Johansen Seljelv <48127711+SigSel@users.noreply.github.com>
2022-07-09 10:19:28 +0200

    Made cards clickable
    Clicking on  a cards takes you to the product on vinmonopolet.no in a new tab.

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,26 +1,29 @@
   
 import React from 'react';
-import { Card, CardMedia, CardContent, Typography } from '@material-ui/core';
+import { Card, CardMedia, CardContent, Typography, CardActionArea } from '@material-ui/core';
 
 import useStyles from './styles';
 
+var baseurl = "https://www.vinmonopolet.no/"
 const Product = ({ product }) => {
   const classes = useStyles();
 
   return (
     <Card className={classes.root}>
-      <CardMedia className={classes.media} image={product.images} title={product.name} />
-      <CardContent>
-        <div className={classes.cardContent}>
-          <Typography gutterBottom variant="h6" component="h2">
-            {product.name}
-          </Typography>
-          <Typography gutterBottom variant="h5" component="h2">
-            {product.price}
-          </Typography>
-        </div>
-        <Typography dangerouslySetInnerHTML={{ __html: product.product_selection }} variant="body2" color="textSecondary" component="p" />
-      </CardContent>
+      <CardActionArea target="_blank" href={baseurl + product.url} >
+        <CardMedia className={classes.media} image={product.images} title={product.name} />
+        <CardContent>
+          <div className={classes.cardContent}>
+            <Typography gutterBottom variant="h6" component="h2">
+              {product.name}
+            </Typography>
+            <Typography gutterBottom variant="h5" component="h2">
+              {product.price}
+            </Typography>
+          </div>
+          <Typography dangerouslySetInnerHTML={{ __html: product.product_selection }} variant="body2" color="textSecondary" component="p" />
+        </CardContent>
+      </CardActionArea>
     </Card>
   );
 };
